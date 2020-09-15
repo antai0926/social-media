f66de44cc7087fbeedc463785545c5cdc38bf0f1
Brian Li <antai0926@gmail.com>
2020-09-15 19:43:29 +0800

    project init add some page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import home from './pages/Home';
+import signup from './pages/Signup';
+import login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      Hello Antai
+      <BrowserRouter>
+        <Switch>
+          <Route paht="/" component={home} />
+          <Route paht="/login" component={login} />
+          <Route paht="/signup" component={signup} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
