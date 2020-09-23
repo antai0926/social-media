d24bc495578de0b70345587f01264a5f388a863a
Brian Li <antai0926@gmail.com>
2020-09-23 23:14:12 +0800

    use firebase functions not local

--- src/App.js
+++ src/App.js
@@ -28,11 +28,11 @@ import { logoutUser, getUserData } from './redux/actions/userAction';
 
 const theme = createMuiTheme(themeObject);
 
-// axios.defaults.baseURL =
-//   'https://europe-west1-antai-social-media.cloudfunctions.net/api';
-
 axios.defaults.baseURL =
-  'http://localhost:5000/antai-social-media/europe-west1/api';
+  'https://europe-west1-antai-social-media.cloudfunctions.net/api';
+
+// axios.defaults.baseURL =
+//   'http://localhost:5000/antai-social-media/europe-west1/api';
 
 function App() {
   const token = localStorage.FBIdToken;
